e671ef2934ef7bbf10d29f40c7930d24b9b82f24
Diego Linares <diegojeanluck@hotmail.com>
2023-04-25 03:27:13 -0500

    Remove hostname from express server

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ import express from 'express';
 import routingController from './routes/index';
 
 const PORT = env.PORT || 5000;
-const HOSTNAME = env.HOSTNAME || 'localhost';
 
 const app = express();
 
 app.use(express.json());
 routingController(app);
 
-app.listen(PORT, HOSTNAME, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
